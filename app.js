e6eb83c4c8e6c2c03ec59ed7569bf253d18e4424
rakshitavn2005 <rakshitanagaraddi1806@gmail.com>
2025-04-01 10:33:41 +0530

    project completed

--- app.js
+++ app.js
@@ -14,7 +14,9 @@ app.use(cors());
 app.use(morgan('dev'));
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/kleecom')
+
+let MONGODB_URL = 'mongodb+srv://rakshitanagaraddi1806:a0ncOAmM5Id3m4TX@cluster0.sjsdcsm.mongodb.net/?retryWrites=true&w=majority'
+mongoose.connect(MONGODB_URL)
 .then(()=>{
     console.log('db connected');
 }).catch((err)=>{
